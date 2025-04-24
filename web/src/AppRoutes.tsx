58039e8e6e67c02767e2972afe1e0f168e20886a
Samilly <samillynunes19@gmail.com>
2025-04-24 20:53:31 -0300

    style: base page created as a template for other ones

--- web/src/AppRoutes.tsx
+++ web/src/AppRoutes.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import BasePage from "./pages/BasePage";
 
-function AppRoutes(){
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/dashboard" element={<Dashboard/>}  />
-            </Routes>
-
-        </BrowserRouter>
-    );
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BasePage />}>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
